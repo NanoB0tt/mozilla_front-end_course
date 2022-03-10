8cefe55b54b6fdfc3644919263a4828de1c83596
Dbbuk <marianofassikoropecki@gmail.com>
2022-03-10 08:45:02 -0300

    added js first steps module

---
+++ javascript/javascript_first_steps/variable_example/script.js
@@ -0,0 +1,8 @@
+const buttonA = document.querySelector('#button_A');
+const headingA = document.querySelector('#headingA');
+
+buttonA.onclick = () => {
+	const name = prompt('What is your name?');
+	alert(`Hello ${name}, nice to see you!`);
+	headingA.textContent = `Welcome ${name}`;
+}
