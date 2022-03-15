2655d8df65304ba8de9de73cc866ac19270f96c9
Dbbuk <marianofassikoropecki@gmail.com>
2022-03-15 14:05:43 -0300

    finished last project

--- javascript/asynchronous_javascript/sequencing_animations/main.js
+++ javascript/asynchronous_javascript/sequencing_animations/main.js
@@ -13,4 +13,39 @@ const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-//alice1.animate(aliceTumbling, aliceTiming);
+// ==== Using the promise version of "callback hell" ==== //
+
+//const doAnimation1 = alice1.animate(aliceTumbling, aliceTiming).finished;
+
+//doAnimation1.then( () => {
+  //const doAnimation2 = alice2.animate(aliceTumbling, aliceTiming).finished;
+  //doAnimation2.then( () => {
+    //alice3.animate(aliceTumbling, aliceTiming).finished;
+  //});
+//});
+
+// =============== Using promise chain ================ //
+
+//alice1.animate(aliceTumbling, aliceTiming).finished
+  //.then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+  //.then(() => alice3.animate(aliceTumbling, aliceTiming).finished)
+  //.catch( error => console.error(`Animation error ${error}`));
+  
+
+
+// ============= Using async and await =============== //
+
+async function animation() {
+  try {
+    const prom1 = await alice1.animate(aliceTumbling, aliceTiming).finished;
+
+    const prom2 = await alice2.animate(aliceTumbling, aliceTiming).finished;
+
+    const prom3 = alice3.animate(aliceTumbling, aliceTiming).finished;
+  }
+  catch(error) {
+    console.error(`Animation error ${error}`);
+  }
+}
+
+animation();
